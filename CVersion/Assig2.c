45caf99a9c21934d065bb206c395de4d708bc23e
Josh Radich <josh.radich@solv.com.au>
2019-10-20 12:47:37 +0800

    20191020

--- CVersion/Assig2.c
+++ CVersion/Assig2.c
@@ -1,3 +1,7 @@
+//CITS3402 - Assignment 2 
+//Name:  Joshua Radich
+//StudentNo: 22744833
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
@@ -6,20 +10,20 @@
 
 void setArrayToZero(int* arr, int size);
 void initialiseArray(int*** arr, int size);
-void loadFile(int** nodeArray, int nodes, FILE* f);
+void loadFile(int** dataArray, int vertices, FILE* f);
 void printArray(int** arr, int len);
-int processArray(int nodes, int startNode, int current, int* doneList, int** nodeArray, int** dataArray);
-int getLowestThatIsntDone(int startNode,int** nodeArray, int* doneArray, int nodes);
-void printArrayToFile(char* fileName,int nodes, int** arr);
-void getMinAndMax(int nodes,int procs, int rank, int* min, int* max);
+int processArray(int vertices, int startVert, int current, int* doneList, int** resultArray, int** dataArray);
+int getLowestThatIsntDone(int startVert,int** resultArray, int* doneArray, int vertices);
+void printArrayToFile(char* fileName,int vertices, int** arr);
+void getMinAndMax(int vertices,int procs, int rank, int* min, int* max);
 
 int main(int argc, char** argv){
 	
-	MPI_Init(&argc,&argv);
+	MPI_Init(&argc,&argv); //begin
 
-	int nodes=0, min=0, max=0;
+	int vertices=0, min=0, max=0;
 	int* doneList;
-	int** nodeArray;
+	int** resultArray;
 	int** dataArray;
 	int** finalArray;
 	clock_t begin, end;	
@@ -27,8 +31,8 @@ int main(int argc, char** argv){
 	int rank,total;
 	MPI_Request request;
 	MPI_Status status;
-	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
-	MPI_Comm_size(MPI_COMM_WORLD, &total);
+	MPI_Comm_rank(MPI_COMM_WORLD,&rank); //get rank of current node
+	MPI_Comm_size(MPI_COMM_WORLD, &total); // get total number of nodes
 
 	if(rank==0){  //head node
 		FILE* f;
@@ -43,59 +47,62 @@ int main(int argc, char** argv){
 			exit(0);
 		}
 
-		fread(&nodes,sizeof(int),1,f); //get number of nodes from file  
-		MPI_Bcast(&nodes,1,MPI_INT,0,MPI_COMM_WORLD);  //send number of nodes to everyone
-		initialiseArray(&dataArray,nodes); //initialise data array
-		initialiseArray(&finalArray,nodes); //initialise final results array
-		loadFile(dataArray,nodes,f);  // load data into array from file 
+		fread(&vertices,sizeof(int),1,f); //get number of vertices from file  
+		MPI_Bcast(&vertices,1,MPI_INT,0,MPI_COMM_WORLD);  //send number of vertices to all nodes
+		initialiseArray(&dataArray,vertices); //initialise data array
+		initialiseArray(&finalArray,vertices); //initialise final results array
+		loadFile(dataArray,vertices,f);  // load data into array from file 
 		fclose(f);  //close file 
 		begin = clock();  // begin timing 
 		printf("Calculating...\n");
-		MPI_Bcast(&(dataArray[0][0]),nodes*nodes,MPI_INT,0,MPI_COMM_WORLD); //send data array to all nodes 
+		MPI_Bcast(&(dataArray[0][0]),vertices*vertices,MPI_INT,0,MPI_COMM_WORLD); //send data array to all nodes
 		
 	}
-	else{  //for all other available nodes 
-		MPI_Bcast(&nodes,1,MPI_INT,0,MPI_COMM_WORLD); //receive number of nodes from head node 
-		initialiseArray(&dataArray,nodes); //all other nodes initialise data array 
-		MPI_Bcast(&(dataArray[0][0]),nodes*nodes,MPI_INT,0,MPI_COMM_WORLD); //receive array
+	else{  //for all other available nodes
+		MPI_Bcast(&vertices,1,MPI_INT,0,MPI_COMM_WORLD); //receive number of vertices from head node 
+		initialiseArray(&dataArray,vertices); //all other nodes initialise data array 
+		MPI_Bcast(&(dataArray[0][0]),vertices*vertices,MPI_INT,0,MPI_COMM_WORLD); //receive data array
 	}
 	
 	// all nodes including head node 
-	doneList = malloc(sizeof(int)*nodes);  //initialise done list array
-	setArrayToZero(doneList,nodes);        //set it to zero
-	initialiseArray(&nodeArray,nodes);     //initialise temp results array
-	getMinAndMax(nodes,total,rank,&min,&max); // get rows that node is going to process depending on rank 
+	doneList = malloc(sizeof(int)*vertices);  //initialise done list array
+	setArrayToZero(doneList,vertices);        //set it to zero
+	initialiseArray(&resultArray,vertices);     //initialise temp results array
+	getMinAndMax(vertices,total,rank,&min,&max); // get verticies that node is going to process depending on rank 
 
-	if(max>=min){  // if max is less than min, theres more procs than nodes.
+	if(max>=min){  // if max is less than min, that node has not been used as there were more nodes than vertices
 		int next = 0;
 		for(int i=min;i<=max;i++){ //each node processes their assigned rows 
 			next = i;
 			while(next != -1){  //process path for each point 
-				next = processArray(nodes,i,next,doneList,nodeArray,dataArray);
+				next = processArray(vertices,i,next,doneList,resultArray,dataArray);
 			}
-			setArrayToZero(doneList,nodes); //reset done list for next point 
+			setArrayToZero(doneList,vertices); //reset done list for next point 
 		}
 
-		int len = (max - min + 1)*nodes;  //stitch final result array together at head node. 
-		MPI_Gather(&(nodeArray[min][0]),len,MPI_INT,&(finalArray[min][0]),len,MPI_INT,0,MPI_COMM_WORLD);
+		int len = (max - min + 1)*vertices;  //stitch final result array together at head node. 
+		MPI_Gather(&(resultArray[min][0]),len,MPI_INT,&(finalArray[min][0]),len,MPI_INT,0,MPI_COMM_WORLD);
 	}
 
 	if(rank==0){  //head node only 
 		end = clock();
 		double procTimeTaken = (double)(end - begin) / CLOCKS_PER_SEC;
 		printf("Completed in: %f\n",procTimeTaken);  //calculate time taken
-		printArrayToFile(argv[1],nodes,finalArray); //print array to file 
+		printArrayToFile(argv[1],vertices,finalArray); //print array to file 
 	}
 	
 	MPI_Finalize();  //finish 
 }
 
-void getMinAndMax(int nodes,int procs, int rank, int* min, int* max){
-	int toAssign = nodes;
+//based on the rank, the number of nodes and the number of vertices, returns 
+//what vertices the node will be processing. 
+//tries to divide vertices out amoungst nodes as evenly as possible. 
+void getMinAndMax(int vertices,int procs, int rank, int* min, int* max){
+	int toAssign = vertices;
 	int assigned = 0;	
-	assigned = toAssign / procs;
-	toAssign = toAssign % procs;
-	if(rank < toAssign){
+	assigned = toAssign / procs;  //divide verticies amoungst nodes
+	toAssign = toAssign % procs;  // get remainder 
+	if(rank < toAssign){  // divide remainder out evenly 
 		assigned++;
 		*min = rank * assigned;
 		*max = *min + assigned -1;
@@ -106,34 +113,38 @@ void getMinAndMax(int nodes,int procs, int rank, int* min, int* max){
 	}
 }
 
-int processArray(int nodes, int startNode, int current, int* doneList, int** nodeArray, int** dataArray){
+//processes path length between the 'current' vertex and all other vertices.  The vertex length that is the 
+//shortest will then be selected and used until all vertices are exhausted. 
+int processArray(int vertices, int startVert, int current, int* doneList, int** resultArray, int** dataArray){
 	int val=0;
-	for(int i=0;i<nodes;i++){
-		if(i!=startNode){
-			if(doneList[i]!=1){
-				if(dataArray[current][i] != 0){
-					val = nodeArray[startNode][current] + dataArray[current][i];
-					if(nodeArray[startNode][i] == 0 || val < nodeArray[startNode][i]){
-						nodeArray[startNode][i] = val;
+	for(int i=0;i<vertices;i++){
+		if(i!=startVert){ //dont caclulate between start vertex and vertex being processed 
+			if(doneList[i]!=1){  // if vertex hasn't been visited yet 
+				if(dataArray[current][i] != 0){  // if the distance from current to i isn't 0  (no path) 
+					val = resultArray[startVert][current] + dataArray[current][i];  //make value what's currently recorded from start  to current + current to i 
+					if(resultArray[startVert][i] == 0 || val < resultArray[startVert][i]){  //is this is less than what's recorded for start to i then replace 
+						resultArray[startVert][i] = val;
 					}
 				}
 			}
 		}
 	}
 	
-	doneList[current] =1;
-	int next = getLowestThatIsntDone(startNode,nodeArray,doneList,nodes);
+	doneList[current] =1; // get current node as done 
+	int next = getLowestThatIsntDone(startVert,resultArray,doneList,vertices); //get path with the lowest value or -1 if none are available 
 	return next;
 }
 
-int getLowestThatIsntDone(int startNode,int** nodeArray, int* doneArray, int nodes){
+//gets the shortest direct path from the available vertices from the start nodes to another.  
+//returns -1 if all vertices have been exhausted. 
+int getLowestThatIsntDone(int startVert,int** resultArray, int* doneArray, int vertices){
 	int lowest = -1;
 	int node = -1;
-	for(int i=0; i<nodes; i++){
-		if(doneArray[i] != 1){
-			if(nodeArray[startNode][i] !=0){
-				if(lowest==-1||nodeArray[startNode][i]<lowest){
-					lowest = nodeArray[startNode][i];
+	for(int i=0; i<vertices; i++){
+		if(doneArray[i] != 1){ //check if vertex has been processed 
+			if(resultArray[startVert][i] !=0){ // check if path is valid 
+				if(lowest==-1||resultArray[startVert][i]<lowest){
+					lowest = resultArray[startVert][i];  //save as lowest 
 					node = i;
 				}
 			}
@@ -142,42 +153,45 @@ int getLowestThatIsntDone(int startNode,int** nodeArray, int* doneArray, int nod
 	return node;
 }
 
+//initialises a 2d array but using a 1d grid so that parts of the 2d array can easily be sent using MPI 
 void initialiseArray(int*** incArr, int size){
 	int fullSize = size * size;
-	int** arr = malloc(sizeof(int*)*size);
-	int* data = (int*)malloc(sizeof(int)*fullSize);
+	int** arr = malloc(sizeof(int*)*size);  //create 2d array of pointers 
+	int* data = (int*)malloc(sizeof(int)*fullSize);  // create 1d array thats contiguous 
 	
-	for(int i=0;i<fullSize;i++){
+	for(int i=0;i<fullSize;i++){ // set values in 1d array to 0
 		data[i]=0;
 	}
 
-	for(int i=0; i<size; i++){
+	for(int i=0; i<size; i++){  //for each row cell in 2d array, assign it the address of the 1d array that correlates to the start of the row 
 		arr[i] = &(data[size*i]);
 	}
-	*incArr = arr;	
+	*incArr = arr;	// assign 2d array to array sent to function 
 }
 
+//sets 1d array values to 0 
 void setArrayToZero(int* arr, int size){
 	for(int i=0;i<size;i++){
 		arr[i]=0;
 	}
 }
 
-void loadFile(int** nodeArray, int nodes, FILE* f){
+//assigns numbers in file to a 2d array 
+void loadFile(int** resultArray, int vertices, FILE* f){
 	
-	int tot = nodes * nodes;
 	int val;
-	for(int i=0;i<nodes;i++){
-		for(int j=0;j<nodes;j++){
-			if(fread(&val,sizeof(int),1,f)!=1){
+	for(int i=0;i<vertices;i++){
+		for(int j=0;j<vertices;j++){
+			if(fread(&val,sizeof(int),1,f)!=1){  
 				printf("Not enough values in file\n");
 				exit(0);
 			}
-			nodeArray[i][j] = val;
+			resultArray[i][j] = val;
 		}
 	}
 }
 
+//prints 2d array to screen.  Was used for testing 
 void printArray(int** arr, int len){
 	for(int i=0;i<len;i++){
 		for(int j=0;j<len;j++){
@@ -187,25 +201,27 @@ void printArray(int** arr, int len){
 	}
 }
 
-void printArrayToFile(char* fileName,int nodes, int** arr){
+//opens a file and prints array to that file 
+void printArrayToFile(char* fileName,int vertices, int** arr){
 	char outName[100];
 	int i=0;
-	while(fileName[i] != '.'){
+	
+	while(fileName[i] != '.'){ //find dot(.) in incoming file name first to make the same named .out file. 
 		outName[i] = fileName[i];
 		i++;
 	}
 	outName[i] = '\0';
-	strcat(outName,".out");
+	strcat(outName,".out"); //add .out to file name. 
 	
-	FILE* f= fopen(outName,"w");
+	FILE* f= fopen(outName,"w"); //open file. 
 	if(f==NULL){
 		printf("Unable to create output file\n");
 		exit(0);
 	}
 	
-	fprintf(f,"%d\n",nodes);
-	for(int i=0;i<nodes;i++){
-		for(int j=0;j<nodes;j++){
+	fprintf(f,"%d\n",vertices);  //print vertices to file first 
+	for(int i=0;i<vertices;i++){
+		for(int j=0;j<vertices;j++){  // print array to file. 
 			fprintf(f,"%d ",arr[i][j]);
 		}
 		fprintf(f,"\n");
